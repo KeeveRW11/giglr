ac6f7f2558440d42a928bce38cdf149ed1c4ff25
Shantanu <97823352+ShawnMaz@users.noreply.github.com>
2022-10-10 00:09:05 -0400

    updated the routes and the dashboard view to show all the posts by a user

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,111 +1,123 @@
-const router = require('express').Router();
-const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Post, User, Comment } = require("../models");
+const withAuth = require("../utils/auth");
 
-
-router.get('/', (req,res)=>{
-
-    Post.findAll({
-        attributes: [
-            'id',
-            'post_url',
-            'title',
-            'created_at',
-            [sequelize.literal('(SELECT COUNT (*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-             //[sequelize.literal('(SELECT COUNT (*) FROM downvote WHERE post.id=downvote.post_id)'), 'downvote_count]?
-             //pull image/file; cloudinary
-        ],
-        include: [
-            {
-                model: Comment,
-                attributes: ['id','comment_text','post_id','user_id','created_at'],
-                include: {
-                    model: User,
-                    attributes: ['username']
-                }
-            },
-            {
-                model: User,
-                attribtues: ['username']
-            }
-        ]
-    })
-        .then(dbPostData=> {
-            
-            const posts = dbPostData.map(post=>post.get({plain: true}));
-            console.log(dbPostData[0]);
-            res.render('homepage', {posts, loggedIn: req.session.loggedIn, homePage: true});
-        })
-        .catch(err=>{
-            console.log(err);
-            res.status(500).json(err)
-        })
-});
-
-router.get('/login', (req, res) => {
-
-    if(req.session.loggedIn){
-        res.redirect('/');
-        return;
-    }
-
-    res.render('login', {login:true});
-});
-
-router.get('/signup', (req, res) => {
-    res.render('signup', {login:true});
-})
-
-router.get('/upload', withAuth, (req, res) => {
-    res.render('upload', {upload:true});
-})
-router.get('/post/:id', (req,res)=>{
-   
-    Post.findOne({
-        where: {
-            id: req.params.id
+router.get("/", (req, res) => {
+  Post.findAll({
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+      [
+        sequelize.literal(
+          "(SELECT COUNT (*) FROM vote WHERE post.id = vote.post_id)"
+        ),
+        "vote_count",
+      ],
+      //[sequelize.literal('(SELECT COUNT (*) FROM downvote WHERE post.id=downvote.post_id)'), 'downvote_count]?
+      //pull image/file; cloudinary
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
         },
-        attributes:[
-            'id',
-            'post_url',
-            'title',
-            'created_at',
-            [sequelize.literal('(SELECT COUNT (*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-        ],
-        include: [
-            {
-                model: Comment,
-                attributes: ['id','comment_text', 'post_id'],
-                include: 
-                    {
-                        model: User,
-                        attributes: ['username']
-                    }
-                
-            },
-            {
-                model: User,
-                attributes: ['username']
-            }
-        ]
+      },
+      {
+        model: User,
+        attribtues: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      console.log(dbPostData[0]);
+      res.render("homepage", {
+        posts,
+        loggedIn: req.session.loggedIn,
+        homePage: true,
+      });
     })
-        .then(dbPostData => {
-            if(!dbPostData){
-                res.status(404).json({message: 'No post found with this id'});
-                return
-            }
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
-            //serialize the data
-            const post = dbPostData.get({plain: true});
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
 
-            //pass data to template
-            res.render('single-post', {post, loggedIn: req.session.loggedIn, singlePage:true});
-        })
-        .catch(err=>{
-            console.log(err);
-            res.status(500).json(err);
-        })
-})
+  res.render("login", { login: true });
+});
+
+router.get("/signup", (req, res) => {
+  res.render("signup", { login: true });
+});
+
+router.get("/upload", withAuth, (req, res) => {
+  res.render("upload", { upload: true });
+});
+
+router.get("/post/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+      [
+        sequelize.literal(
+          "(SELECT COUNT (*) FROM vote WHERE post.id = vote.post_id)"
+        ),
+        "vote_count",
+      ],
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+
+      //serialize the data
+      const post = dbPostData.get({ plain: true });
+
+      //pass data to template
+      res.render("single-post", {
+        post,
+        loggedIn: req.session.loggedIn,
+        singlePage: true,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
