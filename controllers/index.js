7c787cacdca23e580d133cf2d6a27901fd297d90
Shantanu <97823352+ShawnMaz@users.noreply.github.com>
2022-10-10 15:42:53 -0400

    added the dashborad-routes back to the controller for dashboard page to work

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
 const uploadRoutes = require('./upload-routes.js');
+const dashboard = require('./dashboard-routes');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
-router.use('/upload', uploadRoutes)
+router.use('/upload', uploadRoutes);
+router.use('/dashboard', dashboard);
 
 router.use((req,res)=> {
     res.status(404).end();
