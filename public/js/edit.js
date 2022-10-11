822e55a3f7231ac8a2fe740dc95627f91b85e308
Bryan Dumpit <dumpitbr@gmail.com>
2022-10-11 18:09:08 -0400

    added edit.js update and delete functionality, updated dashboard.handlebars for better eventlistener handling on multiple elements with same class rather than same id

---
+++ public/js/edit.js
@@ -0,0 +1,48 @@
+async function editFormHandler (event) {
+    event.preventDefault();
+    console.log(event)
+
+    console.log(event.target);
+
+    const title = event.target.querySelector('input[name="post-title"]').value.trim();
+    const idContainer = event.target.querySelector('div[class="card border border-dark"]');
+    const id = idContainer.getAttribute('id');
+    
+    console.log(title, id)
+    if (event.submitter.id === 'save-button') {
+        
+        const response = await fetch (`api/posts/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify({
+                title
+            }),
+            headers: {
+                'Content-Type': 'application/JSON'
+            }
+        })
+    
+        if (response.ok) {
+            document.location.replace('/dashboard')
+        } else {
+            alert(response.statusText)
+        }
+    } else if (event.submitter.id === 'delete-button') {
+        
+        const response = await fetch (`api/posts/${id}`, {
+            method: 'DELETE',
+        })
+    
+        if (response.ok) {
+            document.location.replace('/dashboard')
+        } else {
+            alert(response.statusText)
+        }
+    }
+    
+
+}
+
+document.querySelectorAll('.edit-meme-form').forEach(item=>{
+    item.addEventListener('submit', editFormHandler)
+});
+
