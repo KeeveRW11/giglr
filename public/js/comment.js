431763d7f5ed1698a90dd8e5828836873546c879
Shantanu <97823352+ShawnMaz@users.noreply.github.com>
2022-10-11 20:58:50 -0400

    removed unnecessary comments and console log from /public/comment/js

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,22 +1,15 @@
-/// single post page and dashboard. If logged in you can comment from single post page
-// will need to check were we are looking for click in the modal
 let post_id 
 
 async function commentFormHandler(event) {
     event.preventDefault();
   
     const comment_text = document.querySelector('input[name="comment-text"]').value.trim();
-    console.log(comment_text);
-    // //const post_id = window.location.toString().split('/')[
-    //   window.location.toString().split('/').length - 1
-    // ];
   
     if (comment_text) {
         const response = await fetch('/api/comments', {
             method: 'POST',
             body: JSON.stringify({
-                comment_text,
-                //user_id: 1,  
+                comment_text,  
                 post_id                              
             }),
             headers: {
@@ -39,7 +32,6 @@ async function commentClickHandler(event) {
 
     if (commentButton.matches('#comment-btn')) {
         post_id = commentButton.dataset.id
-        console.log(post_id);
     }
     
 }  
